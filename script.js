3934d9a0dacb543c52ef975532116e0a1ef056af
Karolmaj0rov <nurmuhammadjonazimov@gmail.com>
2024-08-05 17:06:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let num = Math.random()*100
+
+let num6 = Math.floor(num)
+
+alert(num6)
+
+let misol = `Random raqamni 2 ga ko'paytirib 2 ga bo'ling`
+
+alert(misol)
+
+let num5 = Math.floor(num)
+
+let num2 = num6*2
+
+let num4 = Math.floor(num2)
+
+let num3 = num2/2
+
+let num1 = Math.floor(num3)
+
+alert(num1)
+
+console.log(num1);
